6da620df00a455a1a6249af8f9735ce9f5cf5dbc
Jonathan <jonathansharon3@gmail.com>
2025-04-19 16:50:47 +0300

    vite bug

--- client/vite-project/vite.config.js
+++ client/vite-project/vite.config.js
@@ -3,7 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    copyPublicDir: true
+  },
   server: {
     port: 5173,
+    historyApiFallback: true
   },
 }); 
