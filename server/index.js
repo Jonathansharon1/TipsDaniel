39b08ba6715f99a826610654b68eafee7422ff2b
Jonathan <jonathansharon3@gmail.com>
2025-04-19 18:47:31 +0300

    welcome

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'https://tipsdaniel.onrender.com',
+  origin: ['https://tipsdaniel-client-static.onrender.com', 'http://localhost:5173'],
   credentials: true
 }));
 app.use(express.json());
